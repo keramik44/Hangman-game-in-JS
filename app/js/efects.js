95499976bcc3e2da1181482241d80f024e094db7
keramik44 <keramik@o2.pl>
2017-01-12 21:01:48 +0100

    set-up a project
    just preparing a project

---
+++ app/js/efects.js
@@ -0,0 +1,8 @@
+'use strict';
+
+console.log('Hello');
+
+var x = function x(_x) {
+  return _x * _x;
+};
+console.log(x(12));
---
+++ app/jsDev/efects.js
@@ -0,0 +1,4 @@
+console.log('Hello');
+
+let x = (x)=> x*x;
+console.log(x(12));
